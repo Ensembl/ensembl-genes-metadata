3dc6bf9c180614b56d957330cd24203383fa1fc6
EreboPSilva <ereboperezsilva@gmail.com>
2024-01-30 14:10:45 +0000

    finalizing and cleaning up

--- src/python/ensembl/genes/metadata/transcriptomic/get_metadata.py
+++ src/python/ensembl/genes/metadata/transcriptomic/get_metadata.py
@@ -12,68 +12,113 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""
-Get metadata for transcriptomic registry module
+"""Script to produce a json file per given run, containing all accessions, in the format:
 
-This script simply gathers all metadata required to be added to out 
-transcriptomic database, parses it from the ENA database, and returns a json
-to be parsed into the final DB.
+{
+    table : [
+        metakey : metavalue,
+        ...
+       ]
+},...
 
-parameters
-----------
-arg1    : string
-    string with run_accession to search
-
-returns
--------
-stout   : string
-    contains a simplified json in the agreed upon format to fill the DB
+Returns:
+    str : json element containing metadata for given run
 """
 
 import argparse
 import requests
+import json
 
-def _request_data(run_accession) -> str:
-    """
-    Request the data from ENA site
+def _request_data(run_accession,fields) -> str:
+    """This function, makes a HTTP request for the metadata of the given run_accession.
 
-    parameters
-    ----------
-    accession_run : string
-        accession id for run
+    Args:
+        run_accession (str): unique run aceession
+        fields (array): list of fields of interest to have
 
-    returns
-    -------
-    tsv_list : string
-        list of requeste data for a given accession_run
+    Returns:
+        str: tsv file containing said metadata
     """
 
     query = 'run_accession="' + run_accession + '"'
 
-    print(query)
-
     data = {
     'result': 'read_run',
     'query': query,
-    'fields': 'run_accession,center_name,sample_accession,description,cell_line,cell_type,cultivar,ecotype,experiment_alias,experiment_title,isolate,library_name,sample_alias,sample_description,serotype,strain,study_accession,tax_id,tissue_lib,tissue_type',
+    'fields': ",".join(fields),
     'format': 'tsv',
     }
 
-    response = requests.post('https://www.ebi.ac.uk/ena/portal/api/search', data=data)
+    request = requests.post('https://www.ebi.ac.uk/ena/portal/api/search', data=data)
+    response = request.text
+
     return response
 
-#def _json_parse()
+def _json_parse(response,fields) -> str:
+    """Parse response from http request into a readable json
+
+    Args:
+        data (str): tsv from the http request
+        fields (array): list of fields of interest to have
+
+    Returns:
+        str: json formatted dictionary of metadata and values
+    """
+    
+    table = response.split('\n')
+    data = table[1].split('\t')
+
+    data_dict = {}
+    for key in fields:
+        for value in data:
+            data_dict[key] = value
+            data.remove(value)
+            break
+
+    return json.dumps(data_dict)
 
 def main() -> None:
     """Module's entry-point."""
-    print("Parsing arguments...")
+
+    fields = (
+        'run_accession',
+        'tax_id',
+        'sample_accession',
+        'study_accession',
+        'library_strategy',     # read_type (it refers to wether a run is rna-seq, wgs, ...)
+        'instrument_platform',  # platform
+        'library_layout',       # paired
+        'experiment_alias',     # \
+        'experiment_title',     #  \ go to the same place
+        'library_source',       # library_name (name is requested later, but it's often empty so we'll catch it still to fill tissue-related info if available but best to use source for this field)
+        'library_selection',
+        'tissue_type',          # \
+        'tissue_lib',           #  \ these both work towards filling the same field
+        'cell_line',            # \ 
+        'cell_type',            #  \ same as before
+        'library_name',         # see library_source note
+        'strain',
+        'cultivar',             # \
+        'ecotype',              #  \
+        'isolate',              #   \ these last ones are to cater for the different ways of expresin "strains"
+        'cage_protocol',        # this could be important to some farmed data, so added
+        'center_name'
+    )
+
     parser = argparse.ArgumentParser(prog='get_metadata.py',description='Get metadata for a given run accession from the ENA site')
     parser.add_argument(
-        "run_accession",
-        type=str,
+        '--run',
+        '-r',
+        default="SRR4240445",
         help="Run accession to grab metadata from",
     )
-    args = parser.parse_args()
-    print("Requesting data...")
-    data = _request_data(args.run_accession)
-#    json = _json_parse(data)
+    args = parser.parse_args(['-r', 'SRR4240445'])
+    
+    data = _request_data(args.run,fields)
+    json = _json_parse(data,fields)
+
+    return json # not sure if this is it, or for nextflow to grab the output it has to be printed... if it needs printing, next line would solve it...
+    # print(json)
+
+if __name__ == "__main__":
+    main()
