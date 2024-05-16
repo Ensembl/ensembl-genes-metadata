72da0291dc77d4b37ee3e408e120507860331ed6
‘ens-ftricomi’ <ftricomi@ebi.ac.uk>
2024-05-16 12:40:39 +0100

    updated schema

--- src/mysql/create_db.sql
+++ src/mysql/create_db.sql
@@ -1,16 +1,10 @@
 create database transcriptomic_hackathon;
 use transcriptomic_hackathon;
-create table meta (
-	meta_id int not null auto_increment,
-	taxon_id int not null,
-	last_check DATE,
-	primary key (meta_id)
-);
 create table data_files (
 	file_id int not null auto_increment,
 	run_id int not null,
-	name varchar(255) not null,
-	url varchar(255) not null,
+	file_name varchar(255) not null,
+	file_url varchar(255) not null,
 	md5 varchar(255) not null,
 	basic_statistics SET("PASS","FAIL","WARN"),
 	per_base_sequence_quality SET("PASS","FAIL","WARN"),
@@ -24,8 +18,7 @@ create table data_files (
 	adapter_content SET("PASS","FAIL","WARN"),
 	total_sequences int,
 	gc_content int,
-	read_count int,
-	read_length int,
+	sequence_length int,
 	primary	key (file_id)
 );
 create table study (
@@ -45,16 +38,14 @@ create table run (
 	run_id int not null auto_increment,
 	taxon_id int not null,
 	run_accession varchar(255) not null,
-	species_taxon_id int not null,
-	genus_taxon_id int,
-	qc_status SET("NOT_CHECKED","QC_PASS","QC_FAIL","ALIGNED"),
+	qc_status SET("NOT_CHECKED","FILE_ISSUE","QC_PASS","QC_FAIL","ALIGNED"),
 	sample_accession varchar(255) not null,
 	study_accession varchar(255) not null,
 	read_type varchar(255),
 	platform varchar(255),
 	paired bool not null,
 	experiment varchar(255),
-	description text,
+	run_description text,
 	library_name varchar(255),
 	library_selection varchar(255),
 	tissue varchar(255),
@@ -63,4 +54,3 @@ create table run (
 	strain varchar(255),
 	primary key (run_id)
 );
-
