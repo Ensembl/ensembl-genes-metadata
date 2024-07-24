efeeee08afa3f1d64223c29930871c9ceb13c587
Vianey <vianey@ebi.ac.uk>
2024-07-24 16:24:50 +0100

    Update tables config

--- src/python/ensembl/genes/metadata/db_table_conf.py
+++ src/python/ensembl/genes/metadata/db_table_conf.py
@@ -2,8 +2,8 @@
 
 TABLE_CONF = {
     "assembly": {"method": "per_col", "dkey":"None", "ukey": "None"},
-    "organism": {"method": "per_col", "dkey":"None", "ukey": "None"},
+    "organism": {"method": "per_col", "dkey":"assembly_id", "ukey": "organism_id"},
     "species": {"method": "per_col", "dkey":"None", "ukey": "None"},
     "assembly_metrics" :  {"method": "per_row", "dkey":"assembly_id", "ukey": "None"},
-    "bioproject_lineage": {"method": "per_row_key", "dkey":"assembly_id", "ukey": "None"},
+    "bioproject": {"method": "per_row_key", "dkey":"assembly_id", "ukey": "None"},
     }
