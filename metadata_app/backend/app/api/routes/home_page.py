83c358c89d93261bc6ef150240f2beee85fe9f19
Anna Lazar <lazar@ebi.ac.uk>
2025-05-07 14:53:03 +0100

    New summary feature API

--- metadata_app/backend/app/api/routes/home_page.py
+++ metadata_app/backend/app/api/routes/home_page.py
@@ -1,16 +1,29 @@
 # app/api/routes/home_page.py
+import logging
+from datetime import date
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Optional
 
-from fastapi import APIRouter
 from metadata_app.backend.app.services.home_page_service import get_annotation_counts_by_bioproject
 from metadata_app.backend.app.services.home_page_service import get_assemblies_per_year
 from metadata_app.backend.app.services.home_page_service import get_annotations_per_year
 from metadata_app.backend.app.services.home_page_service import get_metadata_registry_update_dates
 from metadata_app.backend.app.services.home_page_service import get_transcriptomic_registry_update_dates
 from metadata_app.backend.app.services.home_page_service import get_annotations_per_genebuilder
+from metadata_app.backend.app.services.home_page_service import bin_by_genebuild_method
+
 
 
 
 home_page = APIRouter()
+
+
+class FilterInput(BaseModel):
+    bioproject_id: Optional[List[str]] = None
+    release_date: Optional[date] = None
+    taxon_id: Optional[int] = None
+    release_type: Optional[List[str]] = None
 
 
 @home_page.get("/home/bioproject")
@@ -54,3 +67,21 @@ def annotations_per_genebuilder():
     Get counts of assemblies per year.
     """
     return get_annotations_per_genebuilder()
+
+@home_page.post("/home/method_summary")
+def filter_genebuild_summary(filters: FilterInput):
+    try:
+        summary_df = bin_by_genebuild_method(
+            bioproject_id=filters.bioproject_id,
+            release_date=filters.release_date,
+            taxon_id=filters.taxon_id,
+            release_type=filters.release_type
+        )
+        return summary_df
+
+    except HTTPException as e:
+        raise e  # Preserve FastAPI-specific HTTP exceptions
+
+    except Exception as e:
+        logging.exception("Unhandled exception in filter_genebuild_summary")
+        raise HTTPException(status_code=500, detail="Internal server error")
--- metadata_app/backend/app/services/home_page_service.py
+++ metadata_app/backend/app/services/home_page_service.py
@@ -4,8 +4,8 @@ import json
 import logging
 from metadata_app.backend.app.core.database import get_db_connection
 import pandas as pd
-from datetime import datetime, timedelta, date
-
+import datetime
+from metadata_app.backend.app.services.taxonomy_service import get_descendant_taxa
 
 
 def load_bioproject_mapping():
@@ -23,10 +23,10 @@ def get_annotation_counts_by_bioproject():
             cursor = conn.cursor()
             query = """
                 SELECT b.bioproject_id, COUNT(g.assembly_id) AS annotation_count
-				FROM bioproject b
-				JOIN genebuild g ON b.assembly_id = g.assembly_id
-				WHERE g.gb_status != 'in_progress'
-				GROUP BY b.bioproject_id;
+                FROM bioproject b
+                JOIN genebuild g ON b.assembly_id = g.assembly_id
+                WHERE g.gb_status != 'in_progress'
+                GROUP BY b.bioproject_id;
             """
             cursor.execute(query)
             result = cursor.fetchall()
@@ -49,148 +49,217 @@ def get_annotation_counts_by_bioproject():
 
 
 def get_assemblies_per_year():
-	try:
-		with get_db_connection("meta") as conn:
-			cursor = conn.cursor()
-			query = """
+    try:
+        with get_db_connection("meta") as conn:
+            cursor = conn.cursor()
+            query = """
                 SELECT a.assembly_id, a.release_date
                 FROM assembly a
                 WHERE a.is_current = 'current'
             """
-			cursor.execute(query)
-			result = cursor.fetchall()
+            cursor.execute(query)
+            result = cursor.fetchall()
 
-		df = pd.DataFrame(result, columns=["assembly_id", "release_date"])
-		if df.empty:
-			return []
+        df = pd.DataFrame(result, columns=["assembly_id", "release_date"])
+        if df.empty:
+            return []
 
-		# Convert release_date to datetime and extract year
-		df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
-		df['year'] = df['release_date'].dt.year
+        # Convert release_date to datetime and extract year
+        df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+        df['year'] = df['release_date'].dt.year
 
-		# Filter to include only 2019 and later
-		df = df[df['year'] >= 2019]
+        # Filter to include only 2019 and later
+        df = df[df['year'] >= 2019]
 
-		# Group by year and count
-		counts_by_year = df.groupby('year')['assembly_id'].count().reset_index()
-		counts_by_year.columns = ['year', 'assembly_count']
+        # Group by year and count
+        counts_by_year = df.groupby('year')['assembly_id'].count().reset_index()
+        counts_by_year.columns = ['year', 'assembly_count']
 
-		return counts_by_year.to_dict(orient='records')
+        return counts_by_year.to_dict(orient='records')
 
-	except Exception as e:
-		print(f"Error in get_assemblies_per_year: {e}")
-		return []
+    except Exception as e:
+        print(f"Error in get_assemblies_per_year: {e}")
+        return []
 
 def get_annotations_per_year():
-	try:
-		with get_db_connection("meta") as conn:
-			cursor = conn.cursor()
-			query = """
+    try:
+        with get_db_connection("meta") as conn:
+            cursor = conn.cursor()
+            query = """
                 SELECT g.genebuild_id, g.date_completed
                 FROM genebuild g
             """
-			cursor.execute(query)
-			result = cursor.fetchall()
+            cursor.execute(query)
+            result = cursor.fetchall()
 
-		df = pd.DataFrame(result, columns=["genebuild_id", "date_completed"])
-		if df.empty:
-			return []
+        df = pd.DataFrame(result, columns=["genebuild_id", "date_completed"])
+        if df.empty:
+            return []
 
-		# Convert date_completed to datetime and extract year
-		df['date_completed'] = pd.to_datetime(df['date_completed'], errors='coerce')
-		df['year'] = df['date_completed'].dt.year
-		# Filter to include only 2019 and later
-		df = df[df['year'] >= 2019]
+        # Convert date_completed to datetime and extract year
+        df['date_completed'] = pd.to_datetime(df['date_completed'], errors='coerce')
+        df['year'] = df['date_completed'].dt.year
+        # Filter to include only 2019 and later
+        df = df[df['year'] >= 2019]
 
-		# Group by year and count
-		counts_by_year = df.groupby('year')['genebuild_id'].count().reset_index()
-		counts_by_year.columns = ['year', 'annotation_count']
+        # Group by year and count
+        counts_by_year = df.groupby('year')['genebuild_id'].count().reset_index()
+        counts_by_year.columns = ['year', 'annotation_count']
 
-		return counts_by_year.to_dict(orient='records')
+        return counts_by_year.to_dict(orient='records')
 
-	except Exception as e:
-		print(f"Error in get_annotations_per_year: {e}")
-		return []
+    except Exception as e:
+        print(f"Error in get_annotations_per_year: {e}")
+        return []
 
 def get_metadata_registry_update_dates():
-	try:
-		with get_db_connection("meta") as conn:
-			cursor = conn.cursor()
-			query = """
+    try:
+        with get_db_connection("meta") as conn:
+            cursor = conn.cursor()
+            query = """
                 SELECT date_value
                 FROM update_date
                 WHERE update_type = 'regular_update'
             """
-			cursor.execute(query)
-			result = cursor.fetchall()
+            cursor.execute(query)
+            result = cursor.fetchall()
 
-		# Convert result to DataFrame with correct column name
-		df = pd.DataFrame(result, columns=["date_value"])
+        # Convert result to DataFrame with correct column name
+        df = pd.DataFrame(result, columns=["date_value"])
 
-		if df.empty:
-			return []
+        if df.empty:
+            return []
 
-		# Convert to string list of dates
-		return df["date_value"].astype(str).tolist()
+        # Convert to string list of dates
+        return df["date_value"].astype(str).tolist()
 
-	except Exception as e:
-		print(f"Error fetching metadata registry update dates: {e}")
-		return []
+    except Exception as e:
+        print(f"Error fetching metadata registry update dates: {e}")
+        return []
 
 def get_transcriptomic_registry_update_dates():
-	try:
-		with get_db_connection("transcriptomic") as conn:
-			cursor = conn.cursor()
-			query = """
+    try:
+        with get_db_connection("transcriptomic") as conn:
+            cursor = conn.cursor()
+            query = """
                 SELECT last_check
                 FROM meta
             """
-			cursor.execute(query)
-			result = cursor.fetchall()
+            cursor.execute(query)
+            result = cursor.fetchall()
 
-		# Convert result to DataFrame with correct column name
-		df = pd.DataFrame(result, columns=["last_check"])
+        # Convert result to DataFrame with correct column name
+        df = pd.DataFrame(result, columns=["last_check"])
 
-		if df.empty:
-			return []
+        if df.empty:
+            return []
 
-		# Convert to string list of dates
-		return df["last_check"].astype(str).tolist()
+        # Convert to string list of dates
+        return df["last_check"].astype(str).tolist()
 
-	except Exception as e:
-		print(f"Error fetching transcriptomic registry update dates: {e}")
-		return []
+    except Exception as e:
+        print(f"Error fetching transcriptomic registry update dates: {e}")
+        return []
 
 def get_annotations_per_genebuilder():
-	try:
-		with get_db_connection("meta") as conn:
-			cursor = conn.cursor()
-			query = """
+    try:
+        with get_db_connection("meta") as conn:
+            cursor = conn.cursor()
+            query = """
                 SELECT g.genebuild_id, g.date_completed, g.genebuilder
                 FROM genebuild g
             """
-			cursor.execute(query)
-			result = cursor.fetchall()
+            cursor.execute(query)
+            result = cursor.fetchall()
 
-		df = pd.DataFrame(result, columns=["genebuild_id", "date_completed", "genebuilder"])
+        df = pd.DataFrame(result, columns=["genebuild_id", "date_completed", "genebuilder"])
 
-		if df.empty:
-			return []
+        if df.empty:
+            return []
 
-		# Filter to last 30 days
-		logging.info(f"getting timeframe")
-		thirty_days_ago = (datetime.now() - timedelta(days=30)).date()
-		df["date_completed"] = pd.to_datetime(df["date_completed"]).dt.date
-		recent_df = df[df["date_completed"] >= thirty_days_ago]
-		logging.info(f"filtered last 30 days")
-		logging.debug(print(recent_df))
+        # Filter to last 30 days
+        logging.info(f"getting timeframe")
+        thirty_days_ago = (datetime.now() - datetime.timedelta(days=30)).date()
+        df["date_completed"] = pd.to_datetime(df["date_completed"]).dt.date
+        recent_df = df[df["date_completed"] >= thirty_days_ago]
+        logging.info(f"filtered last 30 days")
+        logging.debug(print(recent_df))
 
-		# Group by genebuilder and count
-		count_df = recent_df.groupby("genebuilder").size().reset_index(name="annotations")
+        # Group by genebuilder and count
+        count_df = recent_df.groupby("genebuilder").size().reset_index(name="annotations")
 
-		return count_df.to_dict(orient='records')
+        return count_df.to_dict(orient='records')
 
-	except Exception as e:
-		# Optionally log or re-raise
-		print(f"Error in get_annotations_per_genebuilder: {e}")
-		return []
+    except Exception as e:
+        # Optionally log or re-raise
+        print(f"Error in get_annotations_per_genebuilder: {e}")
+        return []
+
+
+
+def bin_by_genebuild_method(bioproject_id, release_type, taxon_id, release_date):
+    """Bins assemblies based on genebuild.method."""
+    try:
+        with get_db_connection("meta") as conn:
+            cursor = conn.cursor()
+            # Build query conditions
+            conditions = []
+            params = []
+
+            if bioproject_id:
+                conditions.append(f"b.bioproject_id IN ({','.join(['%s'] * len(bioproject_id))})")
+                params.extend(bioproject_id)
+                logging.info(f"Filtering by BioProject IDs: {', '.join(bioproject_id)}")
+
+            if release_date:
+                if isinstance(release_date, pd.Timestamp):
+                    release_date = release_date.strftime('%Y-%m-%d')
+                elif isinstance(release_date, (datetime.date, datetime.datetime)):
+                    release_date = release_date.strftime('%Y-%m-%d')
+                conditions.append("g.release_date_beta >= %s")
+                params.append(release_date)
+                logging.info(f"Filtering by release date: {release_date}")
+
+            if taxon_id:
+                descendant_taxa = get_descendant_taxa(taxon_id)
+                if not descendant_taxa:
+                    logging.error(f"No descendant taxon IDs found for {taxon_id}.")
+                    return f"No descendant taxa found for Taxon ID {taxon_id}.", None, None, None, None
+                conditions.append(f"a.lowest_taxon_id IN ({','.join(['%s'] * len(descendant_taxa))})")
+                params.extend(descendant_taxa)
+                logging.info(f"Filtering by lowest taxon ID: {', '.join(str(id) for id in descendant_taxa)}")
+
+            if release_type:
+                conditions.append(f"gb.release_type IN ({','.join(['%s'] * len(release_type))})")
+                params.extend(release_type)
+                logging.info(f"Filtering by Release Type: {', '.join(release_type)}")
+
+            # If there are conditions, join them with AND; otherwise, select all
+            where_clause = " WHERE " + " AND ".join(conditions) if conditions else ""
+
+            query = f"""
+                    SELECT g.annotation_method, g.release_type, g.release_date_beta, b.bioproject_id, a.lowest_taxon_id
+                    FROM genebuild g
+                    LEFT JOIN assembly a ON g.assembly_id = a.assembly_id
+                    LEFT JOIN bioproject b on g.assembly_id = b.assembly_id
+                    {where_clause};
+                """
+            cursor.execute(query, params)
+            result = cursor.fetchall()
+
+        df = pd.DataFrame(result, columns=["annotation_method", "release_type", "release_date_beta", "bioproject_id", "lowest_taxon_id"])
+
+        if df.empty:
+            return []
+
+
+        # Group by genebuilder and count
+        method_summary = df.groupby('annotation_method', observed=False).size().reset_index(name='number_of_annotations')
+
+        return method_summary.to_dict(orient='records')
+
+    except Exception as e:
+        # Optionally log or re-raise
+        print(f"Error in bin_by_genebuld_method: {e}")
+        return []
+
