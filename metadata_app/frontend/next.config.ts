8117afb529a66c97d13ec9701eb6822706aad305
Anna Lazar <lazar@ebi.ac.uk>
2025-08-05 15:19:47 +0100

    fixed API routes to work in cluster

--- metadata_app/frontend/next.config.ts
+++ metadata_app/frontend/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*', //  FastAPI server
+        destination: 'http://localhost:8000/api/:path*', //  FastAPI server
       },
     ]
   },
