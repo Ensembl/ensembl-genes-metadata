041fb8656540b94dca0c0b521e83dd39fb904b9c
Anna Lazar <lazar@ebi.ac.uk>
2025-09-03 13:40:00 +0100

    Fixed bug. report validation

--- metadata_app/backend/app/models/report_schemas.py
+++ metadata_app/backend/app/models/report_schemas.py
@@ -5,7 +5,7 @@ from datetime import date
 
 class ReportFilterRequest(BaseModel):
     bioproject_id: Optional[List[str]] = None
-    group_name: Optional[str] = None
+    group_name: Optional[List[str]] = None
     start_date: Optional[date] = None
     end_date: Optional[date] = None
     taxon_id: Optional[List[int]] = None
