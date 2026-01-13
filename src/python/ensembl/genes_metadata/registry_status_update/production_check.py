611f8c0d646a1a07b26ec60e39476db06dfec3e5
Anna Lazar <lazar@ebi.ac.uk>
2026-01-13 15:37:16 +0000

    Updated production query

--- src/python/ensembl/genes_metadata/registry_status_update/production_check.py
+++ src/python/ensembl/genes_metadata/registry_status_update/production_check.py
@@ -51,28 +51,24 @@ def check_status_production_db(gca_tuple):
 
     try:
         production_query = f"""
-            SELECT 
+            SELECT
                 assembly.accession AS gca_accession,
                 dataset.status,
                 ensembl_release.release_date,
                 dataset_attribute.value,
                 dataset_attribute.attribute_id
             FROM assembly
-            LEFT JOIN genome
-                ON assembly.assembly_id = genome.assembly_id
-            LEFT JOIN genome_dataset
-                ON genome.genome_id = genome_dataset.genome_id
-            LEFT JOIN dataset
-                ON genome_dataset.dataset_id = dataset.dataset_id
-            LEFT JOIN dataset_attribute
-                ON dataset.dataset_id = dataset_attribute.dataset_id
-            LEFT JOIN dataset_source
-                ON dataset.dataset_source_id = dataset_source.dataset_source_id
-            LEFT JOIN ensembl_release on genome_dataset.release_id = ensembl_release.release_id
+            JOIN genome ON assembly.assembly_id = genome.assembly_id
+            JOIN genome_release on genome_release.genome_id = genome.genome_id
+            join ensembl_release on genome_release.release_id = ensembl_release.release_id
+            JOIN genome_dataset ON genome.genome_id = genome_dataset.genome_id
+            JOIN dataset ON genome_dataset.dataset_id = dataset.dataset_id
+            JOIN dataset_attribute ON dataset.dataset_id = dataset_attribute.dataset_id
+            join attribute on dataset_attribute.attribute_id = attribute.attribute_id
             WHERE dataset.name = "genebuild"
                 AND assembly.accession IN {gca_tuple}
                 AND genome_dataset.is_current = 1
-                AND dataset_attribute.attribute_id IN (71, 169, 37, 34)
+                AND attribute.name IN ("genebuild.last_geneset_update", "genebuild.method", "genebuild.version", "genebuild.annotation_source")
                 AND ensembl_release.release_type = "partial"
         """
 
